14deec02cb5178ad82bee0911d10fcc052c883ad
Edenbdd <aubertra@student.42.fr>
2025-01-29 17:08:45 +0100

    start of mutexes

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,20 +6,27 @@
 /*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:26:46 by aubertra          #+#    #+#             */
-/*   Updated: 2025/01/29 16:32:10 by aubertra         ###   ########.fr       */
+/*   Updated: 2025/01/29 17:04:38 by aubertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int mails = 0;
+typedef struct {
+	int				*mails;
+	pthread_mutex_t	*mutex;
+} thread_data_t;
 
-void	*routine()
+
+void	*routine(void *data)
 {
+	thread_data_t *arg = (thread_data_t *)data;
 	int	i = 0;
-	while (i < 20000000)
+	while (i < 10000000)
 	{
-		mails++;
+		pthread_mutex_lock(arg->mutex);
+		(*(arg->mails))++;
+		pthread_mutex_unlock(arg->mutex);
 		i++;
 	}
 	return (NULL);
@@ -28,19 +35,26 @@ void	*routine()
 
 int main(int argc, char **argv)
 {
-	pthread_t	t1;
-	pthread_t	t2;
-
+	pthread_t		t1;
+	pthread_t		t2;
+	pthread_mutex_t mutex;
+	thread_data_t	data;
+	int				mails;
 	(void)argc;
 	(void)argv;
-	if (pthread_create(&t1, NULL, &routine, NULL))
+	
+	data.mails = &mails;
+	data.mutex = &mutex;
+	pthread_mutex_init(&mutex, NULL); //allocates memory
+	if (pthread_create(&t1, NULL, &routine, &data))
 		return (-1);
-	if (pthread_create(&t2, NULL, &routine, NULL))
+	if (pthread_create(&t2, NULL, &routine, &data))
 		return (-2);
 	if (pthread_join(t1, NULL))
 		return (-3);
 	if (pthread_join(t2, NULL))
 		return (-4);
+	pthread_mutex_destroy(&mutex); // free memory
 	printf("number of mails: %d\n", mails);
     return (0);   
 }
